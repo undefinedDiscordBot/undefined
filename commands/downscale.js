bc190fc6655e46481b6b8668a999c2897d5fa0a7
mmccall0813 <masonjamesmccall0813@gmail.com>
2022-03-24 12:19:40 +0000

    add exact size specifiying to downscale

--- commands/downscale.js
+++ commands/downscale.js
@@ -19,12 +19,19 @@ module.exports = {
             large:255
         }
         var size = sizes[args[0]] || 255;
+        // make square
+        size = {width:size,height:size};
+        if(args[0].match(/[0-2500]x[0-2500]/)){
+            var size = args[0].split("x");
+            size = {
+                width: parseInt(size[0]),
+                height: parseInt(size[1])
+            }
+        }
         var image = message.attachments.first()
-        var resizeParams = {};
-        if(image.width > image.height) resizeParams["width"] = size; else resizeParams["height"] = size;
         Snekfetch.get(image.url).then( (res) => { 
         sharp(res.body)
-        .resize(resizeParams)
+        .resize(size)
         .toFormat("png")
         .toBuffer()
         .then( (output) => {
@@ -34,8 +41,11 @@ module.exports = {
                         attachment: output,
                         name: image.name.startsWith("SPOILER") ? 'SPOILER_output.png' : 'output.png'
                     }
-                ]
-            });
+                ],
+                content: "Here is your downscaled image."
+            }).then( (msg) => {
+                msg.edit(`${msg.content}\n\nOriginal image size: ${image.width}x${image.height}\nDownscaled image size: ${msg.attachments.first().width}x${msg.attachments.first().height}\nOriginal image URL: \`${image.url}\`\nDownscaled image URL: \`${msg.attachments.first().url}\``)    
+            })
         })
     })
     }
